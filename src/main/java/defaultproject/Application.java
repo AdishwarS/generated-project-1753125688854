c58c098b0e0f7804a69dc7762dffdd169c49c45f
AdishwarS <Adishwar.Suresh@infinite.com>
2025-07-21 14:21:31 -0500

    Add src/main/java/defaultproject/Application.java

---
+++ src/main/java/defaultproject/Application.java
@@ -0,0 +1,11 @@
+package defaultproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DefaultprojectApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DefaultprojectApplication.class, args);
+    }
+}
